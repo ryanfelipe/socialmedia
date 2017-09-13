5b8743ab9a01e429b4a1df8cb3e23dcae103dd4c
Hugo Porto <victor.porto7@gmail.com>
2017-09-13 16:58:52 -0300

    13-09-2017-2

--- src/Application/Controller/EditDataPages.php
+++ src/Application/Controller/EditDataPages.php
@@ -1,39 +1,26 @@
 <?php
+
 namespace Application\Controller;
 
 use Geekx\Controller,
     Geekx\Common,
     Geekx\Session;
 
-class Colaborador extends Controller {
+class EditDataPages extends Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->loadModel('Application\Model\Colaborador', 'colaborador');
+        $this->loadModel('Application\Model\EditDataPages', 'editdatapages');
     }
 
     public function main() {
-        if (Session::get("id_usuario")) {
-            Common::redir('index');
-
-        } else {
-            Session::destroy();
-            Common::redir('index');
-        }
+        $this->loadView('software/testes/index');
     }
 
-    //Retorna os dados de todos os colaboradores em um JSON.
-    public function carregaDados() {
-        $limit = (int)filter_input(INPUT_POST, 'max');
-        $offset = (int)filter_input(INPUT_POST, 'init');
+    public function login() {
+        $limit = (int) filter_input(INPUT_POST, 'max');
+        $offset = (int) filter_input(INPUT_POST, 'init');
         $id_usuario = Session::get('id_usuario');
         die(json_encode($this->colaborador->getByIdInJsonLimit($id_usuario, $limit, $offset)));
-    }
-
-    //Usado para acionar a visualização do perfil de um colaborador.
-    public function verColaborador($busca){
-        $id_de = Session::get('id_usuario');
-        $dados['usuario'] = $this->colaborador->verColaborador($busca, $id_de);       
-        
     }
 }
