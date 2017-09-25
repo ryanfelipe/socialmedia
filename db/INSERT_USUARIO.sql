c3f701143c5145af7ccf34127410aec8a3243107
Hugo Porto <victor.porto7@gmail.com>
2017-09-25 15:46:21 -0300

    Adição da pasta db

---
+++ db/INSERT_USUARIO.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario
+	(`nome`, `sobrenome`, `email`, `nascimento`, `minibio`, `genero`, `senha`)
+VALUES 
+	('Victor', 'Porto', 'victor.porto7@gmail.com', '1990-12-07', null, 2, MD5('123'));
