ead67ce097561cf0dc64dbf73f6d736001d061a8
Hugo Porto <victor.porto7@gmail.com>
2017-09-25 13:24:49 -0300

    25-09-2017-1

--- src/Application/Controller/EditDataPages.php
+++ src/Application/Controller/EditDataPages.php
@@ -16,11 +16,4 @@ class EditDataPages extends Controller {
     public function main() {
         $this->loadView('software/testes/index');
     }
-
-    public function login() {
-        $limit = (int) filter_input(INPUT_POST, 'max');
-        $offset = (int) filter_input(INPUT_POST, 'init');
-        $id_usuario = Session::get('id_usuario');
-        die(json_encode($this->colaborador->getByIdInJsonLimit($id_usuario, $limit, $offset)));
-    }
 }
